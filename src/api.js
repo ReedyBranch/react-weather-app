6c133a76aa514f0320607b16418af580b13521a4
ReedyBranch <149740846+ReedyBranch@users.noreply.github.com>
2025-10-29 22:48:01 -0400

    Mask API keys in src/api.js
    Replaced sensitive API keys with placeholders.

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": "9e91368a4fmsh612ff4495500e80p19b227jsn3e155cc96c62",
+    "x-rapidapi-key": "*********************",
     "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "c8872aa1dd9312d31a9649e649ffd595";
+export const WEATHER_API_KEY = "*************";
